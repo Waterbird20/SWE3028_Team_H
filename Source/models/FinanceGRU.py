a245da29c1c372d5c880c6e430da05ca5e4d98ff
Edery <58280210+tearbird99@users.noreply.github.com>
2023-10-26 19:23:10 +0900

    Upload GRU Model Source - FinanceGRU.py

---
+++ Source/models/FinanceGRU.py
@@ -0,0 +1,33 @@
+import torch
+from torch import nn
+
+class FinanceGRU(nn.Module):
+
+    def __init__(self, model_args):
+
+        super(FinanceGRU, self).__init__()
+
+        self.output_length = model_args.output_length
+        self.num_layers = model_args.num_layers
+        self.input_size = model_args.input_size
+        self.hidden_size = model_args.hidden_size
+        self.fc_hidden_size = model_args.fc_hidden_size
+        self.dropout = model_args.dropout
+
+        self.gru = nn.GRU(input_size = self.input_size, hidden_size = self.hidden_size,
+                            num_layers = self.num_layers, dropout = self.dropout, batch_first = True)
+        self.fc1 = nn.Linear(self.hidden_size, self.fc_hidden_size)
+        self.fc2 = nn.Linear(self.fc_hidden_size, self.output_length)
+        self.relu = nn.ReLU()
+
+    
+    def forward(self, x):
+        h_0 = torch.Tensor(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+        output, (hn) = self.gru(x, (h_0))
+        hn = hn.view(-1, self.hidden_size)
+        logits = self.relu(hn)
+        logits = self.fc1(logits)
+        logits = self.relu(logits)
+        logits = self.fc2(logits)
+
+        return logits
