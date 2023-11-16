52bc5274f585a35be795bcb8b02f0632df14bc1b
ckckdud3 <ckckdud123@gmail.com>
2023-11-16 10:07:33 +0900

    [ADD] FinanceDataBuilder (WIP)

--- Source/dataset/FinanceDataset.py
+++ Source/dataset/FinanceDataset.py
@@ -19,6 +19,15 @@ import FinanceDataReader as fdr
     Author : Chan-young Lee
 
 """
+
+us_stable = ['APPL', 'MSFT', 'AMZN', 'NVDA', 'META', 'AVGO', 'GOOGL', 'GOOG', 'TSLA', 'ADBE', 'COST', 'CSCO', 'NFLX', 'AMD']
+us_unstable = ['KO', 'MCD', 'WM', 'RSG', 'PEP', 'CL', 'WMT', 'CBOE', 'GD', 'KMB', 'PG', 'COR', 'IBM']
+
+ko_stable = ['005930', '006400', '033780', '000100', '000660', '005830', '010130', '001450', '138040', '030000', '011070', \
+             '004170', '024100', '036570', '058470', '011170', '004370', '012750', '081660']
+ko_unstable = ['005490', '035420', '005380', '051910', '000270', '068270', '105560', '055550', '373220',  '035720', '012330', \
+               '028260',  '207940', '066570', '086790']
+
 class FinanceDataset(Dataset):
 
     def __init__(self, data_args, mode='train'):
