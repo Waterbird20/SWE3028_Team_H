302f6c1fb267c68b10884e65939c6374fdc6fc11
Donghun Jung <atompioneer@g.skku.edu>
2023-12-03 08:44:57 +0900

    [EDIT] stock_test.html

--- Frontend/static/stock_test.js
+++ Frontend/static/stock_test.js
@@ -1,41 +1,68 @@
 anychart.onDocumentReady(function () {
+  // code = '005930';
+  code = localStorage.getItem("stock_code");
+  localStorage.removeItem('stock_code');
   anychart.data.loadCsvFile(
     // 'https://gist.githubusercontent.com/shacheeswadia/cd509e0b0c03964ca86ae7d894137043/raw/5f336c644ad61728dbac93026f3268b86b8d0680/teslaDailyData.csv',
-    'static/005930.csv',
+    'static/'+ code + '.csv',
     function (data) {
     // create data table on loaded data
+    const num_data = data.split('\n').length;
+
+    localStorage.setItem("tomorrow_high", data.split('\n')[num_data-6].split(",")[2]);
+    localStorage.setItem("tomorrow_low", data.split('\n')[num_data-6].split(",")[3]);
+    localStorage.setItem("today_high", data.split('\n')[num_data-7].split(",")[2]);
+    localStorage.setItem("today_low", data.split('\n')[num_data-7].split(",")[3]);
+    
+    line_data = data.split('\n').slice(0, num_data-6).join('\n');
+    
+
     var dataTable = anychart.data.table();
     dataTable.addData(data);
-  
+
+    var lineTable = anychart.data.table();
+    lineTable.addData(line_data);
+
     // map loaded data for the candlestick series
-    var mapping = dataTable.mapAs({
+    var mapping = lineTable.mapAs({
       open: 1,
       high: 2,
       low: 3,
       close: 4
     });
 
-    var mapping2 = dataTable.mapAs({
-      high: 2,
-      low: 3,
-    });
-  
+
     // create stock chart
     var chart = anychart.stock();
   
     // create first plot on the chart
     var plot = chart.plot(0);
-    
+
+
+
+    // Add a range spline series using the data from the computer
+    var bbandsSeries = chart.plot().rangeSplineArea(dataTable, {
+      high: 2,
+      low: 3
+    });
+
+    // Set name and visual effects
+    bbandsSeries
+    .name('Bands')
+    .fill(anychart.color.lighten('#CDB4DB', 0.7))
+    .highStroke('#FF7F7F')
+    .lowStroke('#ADD8E6');
+
     // set grid settings
     plot.yGrid(true).xGrid(true).yMinorGrid(true).xMinorGrid(true);
-  
+
     var series = plot.candlestick(mapping).name('SAMSUMG ELECTRONICS CO.');
     series.legendItem().iconType('rising-falling');
-    series.risingFill("#FF0D0D");
-    series.risingStroke("#FF0D0D");
-    series.fallingFill("#43FF43");
-    series.fallingStroke("#43FF43");
-
+    series.risingFill("#E34234");
+    series.risingStroke("#E34234");
+    series.fallingFill("#1E90FF");
+    series.fallingStroke("#1E90FF");
+    
     // create scroller series with mapped data
     chart.scroller().candlestick(mapping);
     
